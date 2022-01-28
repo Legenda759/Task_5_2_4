a80df11ea5c1231bf31c041596d54aa2a36c6fae
Legenda759 <oripov1994@gmail.com>
2022-01-29 04:49:55 +0500

    Collection view created and normal UI created

--- Task_5_2_4/ContentView.swift
+++ Task_5_2_4/ContentView.swift
@@ -7,10 +7,26 @@
 
 import SwiftUI
 
+extension UIScreen{
+    static let width = UIScreen.main.bounds.width
+    static let height = UIScreen.main.bounds.height
+}
+
 struct ContentView: View {
+    
+    var columns: [GridItem] = Array(repeating: .init(.fixed(UIScreen.width/2)), count: 2)
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ScrollView{
+            LazyVGrid(columns: columns){
+                ForEach((0...100), id: \.self){ item in
+                    ZStack(alignment: .bottom){
+                        Image("mbp16.2021").resizable().aspectRatio(contentMode: .fit) .frame(width: UIScreen.width/2, height: UIScreen.width/2, alignment: .top)
+                        Text("MacBook Pro \(item)").padding().frame(maxWidth: .infinity, alignment: .center).background(Color.gray)
+                    }.background(.green)
+                }
+            }
+        }
     }
 }
 
